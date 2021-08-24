99221292cd54936cb100098a6b737e1ef9d36597
Professor.dev <professor.dev.run@gmail.com>
2021-08-24 19:16:26 +0430

    setup ts workflow

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+const express = require('express');
+const app = express();
+const invoices = require('./routes/invoices');
+app.use(express.json());
+app.use('/api/invoices', invoices);
+const port = process.env.PORT || 3000;
+app.listen(port, console.log('Server is running...'));
